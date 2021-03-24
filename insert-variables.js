9c73bf0b430e989bc543af67d5e01c5ff905adce
Edwin Linares <elinares@artisplendore.com>
2021-03-24 13:13:10 -0600

    [ Fix ] Fixed issue on where: 1) Templates didn't have any variable

--- insert-variables.js
+++ insert-variables.js
@@ -13,20 +13,22 @@ export default ({htmlString, args }) => {
     const regexKey = /([^{])([^}]+)/g
     const regexLookForTags = /<(\S*?)[^>]*>.*?<\/\1>|<.*?\/>/g 
     let newHTML = htmlString;
-    newHTML.match(regexVars).forEach( variable => {
-        const key = variable.match(regexKey)[0]
-        if(args[key] != null){
-            const htmlTags = args[key].match(regexLookForTags) 
-            const isHTML = htmlTags != null
-            if(isHTML){
-                const destructuredString = [...args[key]]
-                args[key] = '';
-                destructuredString.forEach( (letter, index) => {
-                    args[key] +=  index == 0 ? `${letter} ` : letter == '/' ? ` ${letter}` : letter;
-                })
+    if(newHTML.match(regexVars) != null){
+        newHTML.match(regexVars).forEach( variable => {
+            const key = variable.match(regexKey)[0]
+            if(args[key] != null){
+                const htmlTags = args[key].match(regexLookForTags) 
+                const isHTML = htmlTags != null
+                if(isHTML){
+                    const destructuredString = [...args[key]]
+                    args[key] = '';
+                    destructuredString.forEach( (letter, index) => {
+                        args[key] +=  index == 0 ? `${letter} ` : letter == '/' ? ` ${letter}` : letter;
+                    })
+                }
             }
-        }
-        newHTML = newHTML.replace(/{{([^}]+)}}/,args[key] )
-    })
+            newHTML = newHTML.replace(/{{([^}]+)}}/,args[key] )
+        })
+    } 
     return newHTML;
-}
+} 
